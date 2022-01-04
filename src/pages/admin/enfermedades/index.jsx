e8dc311d8c061da2a3158e4821d9a47f6889a0c6
Marco <marco.fura@unmsm.edu.pe>
2022-01-04 07:49:21 -0500

    testing ssr

--- src/pages/admin/enfermedades/index.jsx
+++ src/pages/admin/enfermedades/index.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
 import LayoutAdmin from "../../../components/layout/LayoutAdmin";
 import { API } from "../../../consts/api";
-
-export default function Enfermedades() {
-  const [enfermedades, setEnfermedades] = useState([]);
-  const fetchData=async()=>{
+import {useForm} from "../../../components/hooks/useForm";
+export default function Enfermedades({enfermedades}) {
+  //const [enfermedades, setEnfermedades] = useState([]);
+  const {form,setForm,handleChange}=useForm({
+    visibility:false,
+    codeEnfermedad:"",
+    nombreEnfermedad:""
+  });
+  
+  /*const fetchData=async()=>{
       try{
     const response=await fetch(`${API}enfermedad`);
     const enfermedades=await response.json();
@@ -18,7 +24,7 @@ export default function Enfermedades() {
   }
   useEffect(()=>{
     fetchData();
-  },[])
+  },[])*/
   return (
     <LayoutAdmin>
       <section>
@@ -54,7 +60,8 @@ export default function Enfermedades() {
                   <td>{enfermedad.nombreEnfermedad}</td>
                   <td>{enfermedad.codeEnfermedad}</td>
                   <td>
-                    <button><i className="fas fa-edit" ></i></button>
+                    <button onClick={()=>setForm({...form,visibility:true,nombreEnfermedad:enfermedad.nombreEnfermedad,codeEnfermedad:enfermedad.codeEnfermedad})
+                    } ><i className="fas fa-edit" ></i></button>
                     <button><i className="fas fa-trash-alt"></i></button>
                   </td>
                   
@@ -63,14 +70,46 @@ export default function Enfermedades() {
                 
               </tbody>
             </table>
+            {form.visibility && 
+            <form>
+              <h2>Editar enfermedad</h2>
+              <input onChange={handleChange} name="codeEnfermedad" value={form.codeEnfermedad} placeholder="Código de la enfermedad" type="text" />
+              <input onChange={handleChange} name="nombreEnfermedad" value={form.nombreEnfermedad} placeholder="Nombre de la enfermedad" type="text" />
+              <article>
+              <button className="btn btn-danger" onClick={()=>setForm({...form,visibility:false})}>Cancelar</button>
+              <button className="btn btn-primary" >Guardar cambios</button>
+              </article>
+            </form>
+            }
           </div>
         </div>
       </section>
       <style jsx>{`
+      h2{
+        font-weight:lighter;
+        text-align:center;
+      }
+      form{
+        box-shadow:0 0.5rem 1.5rem rgba(0,0,0,0.2);
+        display:flex;
+        flex-direction:column;
+        padding:2rem;
+      }
+      form>input{
+        margin:0.5rem;
+        border-radius:0.5rem;
+        width:20rem;
+        padding:0.5rem;
+        border:0.1rem solid rgba(0,0,0,0.2);
+      }
       td>button{
         background-color:transparent;
         color:white;
         border:0;
+      }
+      article{
+        display:flex;
+        justify-content:space-around;
       }
       section{
         padding:1rem;
@@ -80,9 +119,9 @@ export default function Enfermedades() {
       
   );
 }
-/*export async function getServerSideProps(){
+export async function getServerSideProps(){
   try{
-    const response=await fetch("https://atento-salud.vercel.app/api/enfermedad");
+    const response=await fetch(`${API}enfermedad`);
     const enfermedades=await response.json();
     return{
       props:{
@@ -98,4 +137,4 @@ export default function Enfermedades() {
       }
     }
   }
-}*/
+}
